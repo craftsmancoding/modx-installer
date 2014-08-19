b2d4e2c40a9909608aac3013ad13fe67e541e804
Everett Griffiths <everett@fireproofsocks.com>
2014-08-18 23:47:47 -0700

    More testing.

--- src/Composer/Script.php
+++ src/Composer/Script.php
@@ -4,19 +4,24 @@
  * process any packages that are listed as dependencies, so if you are trying to install a 
  * MODX Package that in turn has MODX Packages as dependencies, then the scripts here handle the 
  * root package and the custom installer handles the packages that are listed in the required key.
+ *
+ * See https://getcomposer.org/doc/articles/scripts.md
  */
 namespace ModxInstaller\Composer;
 
 class Script {
-     public static function postInstall() {
+     public static function postInstall(Event $event) {
         error_log(__CLASS__.'::'.__FUNCTION__.' called...');
      }
      
-     public static function postUpdate() {
-        error_log(__CLASS__.'::'.__FUNCTION__.' called...');     
+     public static function postUpdate(Event $event) {
+        error_log(__CLASS__.'::'.__FUNCTION__.' called...');
+        $composer = $event->getComposer();     
+        $installedPackage = $event->getOperation()->getPackage();
+        error_log(print_r($installedPackage,true));
      }
      
-     public static function preUninstall() {
+     public static function preUninstall(Event $event) {
         error_log(__CLASS__.'::'.__FUNCTION__.' called...');     
      }
 }
