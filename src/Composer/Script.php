4e76a50b257bc190ef787104ae2751fb3701af79
Everett Griffiths <everett@fireproofsocks.com>
2014-08-19 13:28:46 -0700

    More testing...

--- src/Composer/Script.php
+++ src/Composer/Script.php
@@ -5,11 +5,72 @@
  * MODX Package that in turn has MODX Packages as dependencies, then the scripts here handle the 
  * root package and the custom installer handles the packages that are listed in the required key.
  *
+ * Composer methods: Array
+(
+[0] => setPackage
+[1] => getPackage
+[2] => setConfig
+[3] => getConfig
+[4] => setLocker
+[5] => getLocker
+[6] => setRepositoryManager
+[7] => getRepositoryManager
+[8] => setDownloadManager
+[9] => getDownloadManager
+[10] => setInstallationManager
+[11] => getInstallationManager
+[12] => setPluginManager
+[13] => getPluginManager
+[14] => setEventDispatcher
+[15] => getEventDispatcher
+[16] => setAutoloadGenerator
+[17] => getAutoloadGenerator
+)
+ *
+ * Event methods: Array
+(
+[0] => __construct
+[1] => getOperation
+[2] => getComposer
+[3] => getIO
+[4] => isDevMode
+[5] => getName
+[6] => getArguments
+[7] => isPropagationStopped
+[8] => stopPropagation
+)
+ *
+ * Config methods: Array
+(
+[0] => __construct
+[1] => setConfigSource
+[2] => getConfigSource
+[3] => setAuthConfigSource
+[4] => getAuthConfigSource
+[5] => merge
+[6] => getRepositories
+[7] => get
+[8] => all
+[9] => raw
+[10] => has
+)
  * See https://getcomposer.org/doc/articles/scripts.md
  */
 namespace ModxInstaller\Composer;
 
 class Script {
+
+    /**
+     * @return bool
+     */
+    public static function preInstall(\Composer\Script\PackageEvent $event) {
+        $io = $event->getIO();
+        if ($io->askConfirmation("Are you sure you want to proceed? ", false)) {
+            // ok, continue on to composer install
+            return true;
+        }
+    }
+
      public static function postInstall(\Composer\Script\PackageEvent $event) {
         error_log(__CLASS__.'::'.__FUNCTION__.' called...');
      }
@@ -38,7 +99,9 @@ class Script {
         $methods = get_class_methods($config);
         error_log('Congif methods: '.print_r($methods,true));
 
-        error_log('Congig get: '.$config->get('type'));
+         $src = $config->getConfigSource();
+         error_log('Config contents: '.print_r($src, true));
+        //error_log('Congig get: '.$config->get('type'));
 //        error_log('Composer config '.print_r($config,true));        
 //        error_log(print_r($composer->getPackageBasePath(), true));
      }
